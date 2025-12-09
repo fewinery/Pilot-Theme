ae78c1ca56adeb96df6f53aaaf8051f54c73aee1
fewinery <support@fairwindsestatewinery.com>
2025-12-09 16:13:35 -0600

    Update root.tsx
    fixing issue with semi-colon

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 // Supports weights 400-700
 //import "@fontsource-variable/cabin";
-import "@fontsource/kumbh-sans": "5.0.0"
+import "@fontsource/kumbh-sans": "5.0.0";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import type { SeoConfig } from "@shopify/hydrogen";
 import { Analytics, getSeoMeta, useNonce } from "@shopify/hydrogen";
