97f18851da0daf79f1189ee1f46265d02ce61deb
fewinery <support@fairwindsestatewinery.com>
2026-01-12 10:26:50 -0600

    Update components.ts

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -31,7 +31,6 @@ import * as ImageWithTextImage from "~/sections/image-with-text/image";
 import * as JudgemeReview from "~/sections/judgeme-reviews";
 import * as JudgemeReviewList from "~/sections/judgeme-reviews/review-list";
 import * as JudgemeReviewSummary from "~/sections/judgeme-reviews/review-summary";
-import * as KlaviyoForm from "~/sections/klaviyo-form";
 import * as MainProduct from "~/sections/main-product";
 import * as JudgemeStarsRating from "~/sections/main-product/judgeme-stars-rating";
 import * as ProductATCButtons from "~/sections/main-product/product-atc-buttons";
@@ -110,7 +109,6 @@ export const components: HydrogenComponent[] = [
   Testimonial,
   TestimonialItems,
   TestimonialItem,
-  KlaviyoForm,
   ImageGallery,
   ImageGalleryItems,
   ImageGalleryItem,
